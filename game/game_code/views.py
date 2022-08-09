2c073c8e5fa33b46dbd056567167f9a9b03ad129
kpmn819 <kpneff@comcast.net>
2022-08-09 13:04:13 -0400

    making a list debug works

--- game/game_code/views.py
+++ game/game_code/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+#from game import game
+from .models import Game, Qna
+
 # Create your views here.
-def index(response):
-    return HttpResponse("Any text you want")
+def index(response, id):
+    game_data = Game.objects.get(id=id)
+    return HttpResponse('<h1>%s</h1>' % game_data.name)
 
 def v1(response):
     return HttpResponse("<h1>View 1</h1>")
 
-def home(response):
-    return render(response, 'game_code/home.html', {'somevar':'db stuff'})
+def home(request):
+    #turn_data = Qna.objects.get(all)
+    #turn_data = [{'question':'specs', 'answer':'33'}, {'question':'shoes','answer':'99'}]
+    
+    # get everything
+    query_results = Game.objects.all()
+    # make dictionary
+    turn_data = {'query_results': query_results}
+    print(turn_data)
+    
+    
+    
+    return render(request, 'game_code/home.html', turn_data)
