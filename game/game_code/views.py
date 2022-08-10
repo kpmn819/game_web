9d485cd111702a427cd40d4ef92294d1594b6792
kpmn819 <kpneff@comcast.net>
2022-08-10 11:49:57 -0400

    wins_by_game working

--- game/game_code/views.py
+++ game/game_code/views.py
@@ -18,20 +18,32 @@ def home(request):
     #turn_data = [{'question':'specs', 'answer':'33'}, {'question':'shoes','answer':'99'}]
     
     # get everything
-    query_results = Game.objects.all()
+    #query_results = Game.objects.all()
     total_games = Game.objects.count()
 
     right_5 = Game.objects.filter(score = 5).count()
     right_4 = Game.objects.filter(score = 4).count()
     unfinished = Game.objects.filter(score = -1).count()
+    
+    wins_by_game=[]
+    g_name = Game.objects.all().values_list('name', flat=True).distinct()
+    for item in g_name:
+        by_game_5 = Game.objects.filter(score = 5).count()
+        by_game_5 = 'Perfect score in '+ item + '= '+ str(by_game_5)
+        wins_by_game.append(by_game_5)
+    
+        print(wins_by_game)
+
+    query_results = wins_by_game
+        
 
     print('Total games are: '+ str(total_games))
-    print('Perfect games= ' + str(right_5))
+    print('Perfect games= ' +  str(right_5))
     print('4 out of 5 = ' + str(right_4))
     print('Unfinished games= ' + str(unfinished))
 
     # make dictionary
-    turn_data = {'query_results': query_results}
+    turn_data = {'wins_by_game': wins_by_game}
     #print(turn_data)
     
     
